838d4d80d33cc6478d054a02162765a7900c6020
Emmanuel WETOUMDIE <wetoumdieemmanuellandry@yahoo.fr>
2021-01-18 15:55:51 +0100

    exo1

---
+++ src/app/models/user-options.ts
@@ -0,0 +1,6 @@
+export interface UserOptions {
+  name: string;
+  email: string;
+  photo?: string;
+  active?: boolean;
+}
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,7 +3,20 @@
  * A user logged into the application.
  */
 
+import { UserOptions } from "./user-options";
+
 // Créer la classe User ici.
 export class User {
-  constructor(options: any) {}
+  name: string;
+  email: string;
+  photo: string;
+  active: boolean;
+
+  constructor(options: UserOptions) {
+    this.name = options.name;
+    this.name = options.email;
+    this.photo = options.photo || '';
+    this.active = options.active ? true : options.active;
+  }
+
 }
