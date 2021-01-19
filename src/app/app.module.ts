2421e4cfb794f67319fd1cf624d7fa04221ec8eb
Emmanuel WETOUMDIE <wetoumdieemmanuellandry@yahoo.fr>
2021-01-19 12:19:13 +0100

    exo2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [ AppComponent ],
-  imports: [ BrowserModule ],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
